134b4e5b27080a6f6267aba09037f4c98917150b
Ronnel Tabanera <ronnel.tabanera@gmail.com>
2026-08-29 08:53:50 +0800

    fix api.js

--- src/api.js
+++ src/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
-
 const api = axios.create({
 	baseURL: import.meta.env.VITE_MOVIES_APP_API
 });
 
 api.interceptors.request.use((config) => {
-	const token = localStorage.getItem('token');
-	if(token) {
-		config.headers.Authorization = `Bearer ${token}`;
+	if (!config.headers.Authorization) {
+		const token = sessionStorage.getItem('token');
+		if(token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
 	}
 	return config;
 })
